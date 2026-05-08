2d77431d0551278e939724ae0c02ab5347f11612
Prit Kanani <kananiprit0@gmail.com>
2026-05-08 18:33:16 +0530

    Added Scaler Ui and View implementation for general perpose.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library_Management_System.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
